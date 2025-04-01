51411664efaba027f2632c8f4741415a2e524f1b
a22-air <c5aaa.p3v@icloud.com>
2025-04-02 07:49:37 +0900

    スキルページのレイアウト修正（<li>表示を追加)

--- app/skills/layout.tsx
+++ app/skills/layout.tsx
@@ -1,5 +1,7 @@
+"use client";
 import Sheet from "@/app/_components/Sheet";
 import Hero from "@/app/_components/Hero";
+import { Desktop, RocketLaunch, PencilRuler, PersonArmsSpread } from "@phosphor-icons/react";
 
 type Props = {
   children: React.ReactNode;
--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,38 +1,70 @@
 "use client";
+import React from 'react';
 import { Desktop, RocketLaunch, PencilRuler, PersonArmsSpread } from "@phosphor-icons/react";
+
+type Skills = {
+  id: string;
+  title: string;
+  items:string[];
+}
+
+const data: { contents: Skills[] } = {
+    contents: [
+      {
+        id: "1",
+        title: "プログラミング言語",
+        items:["Swift", "JavaScript","TypeScript", "Java"],
+      },
+      {
+        id: "2",
+        title: "フレームワーク & ライブラリ",
+        items:[ "React", "jQuery","TailWindCSS", "SwiftUI", "Next.js", "Express", "realm.swift" ],
+      },
+      {
+        id: "3",
+        title: "ツール & 環境",
+        items:[ "Xcode", "Git", "Vercel", "Visual", "Studio Code", "RealmSwift", "draw.io" ]
+      },
+      {
+        id: "4",
+        title: "開発経験 & 強み",
+        items: [ "iOSアプリの保守経験（Swift）", "Webページの保守経験（JavaScript）", "APIデータを取得した天気アプリの開発（Swift）", "Todoアプリの開発（React）", "Next.js を用いたポートフォリオサイトの構築" ]
+      }
+    ],
+  }
 
 export default function Page() {
   return (
-    <div className="p-16 bg-white">
-      {/* プログラミング言語 */}
-      <div className="mb-6 text-1xl">
-        <h2 className="text-2xl font-bold text-gray-800 mb-2 flex"><span className="pr-2"><Desktop size={32} color="#0d0c0d" /></span>プログラミング言語</h2>
-        <p className="text-gray-700">Swift / JavaScript / TypeScript / Java </p>
-      </div>
-
-      {/* フレームワーク & ライブラリ */}
-      <div className="mb-6">
-        <h2 className="text-2xl font-bold text-gray-800 mb-2 flex"><span className="pr-2"><RocketLaunch size={32} color="#0d0c0d" /></span>フレームワーク & ライブラリ</h2>
-        <p className="text-gray-700">React / jQuery / TailWindCSS / SwiftUI / Next.js / Express / realm.swift</p>
-      </div>
-
-      {/* ツール & 環境 */}
-      <div className="mb-6">
-        <h2 className="text-2xl font-bold text-gray-800 mb-2 flex"><span className="pr-2"><PencilRuler size={32} color="#0d0c0d" /></span>ツール & 環境</h2>
-        <p className="text-gray-700">Xcode / Git / Vercel / Visual Studio Code / RealmSwift / draw.io </p>
-      </div>
-
-      {/* 開発経験 & 強み */}
-      <div className="mb-6">
-        <h2 className="text-2xl font-bold text-gray-800 mb-2 flex"><span className="pr-2"><PersonArmsSpread size={32} color="#0d0c0d" /></span>開発経験 & 強み</h2>
-        <ul className="list-disc list-inside text-gray-700">
-          <li>iOSアプリの保守経験（Swift）</li>
-          <li>Webページの保守経験（JavaScript）</li>
-          <li>APIデータを取得した天気アプリの開発（Swift） </li>
-          <li>Todoアプリの開発（React）</li>
-          <li>Next.js を用いたポートフォリオサイトの構築</li>
-        </ul>
-      </div>
-    </div>
+    <section className="p-16 bg-white text-black">
+      <dl>
+        {data.contents.map((article) => (
+          <React.Fragment key={article.id}>
+            <dt className="text-2xl font-bold text-gray-800 mb-2 flex">
+               {/* アイコンを読み込むと遅いので後で修正 */}
+            {/* <span className="pr-2"><Desktop size={32} color="#0d0c0d" /></span> */}
+              {article.title}
+            </dt>
+              <div className="flex mb-6">
+              {article.id !== "4" ? (
+                // <dd>で表示
+                article.items.map((item, index) => (
+                  <React.Fragment key={index}>
+                    <dd>{item}</dd>
+                    {index < article.items.length - 1 && <span className="mx-1">/</span>}
+                  </React.Fragment>
+                ))
+              ) : (
+                // 開発経験&強みは<li>で表示
+                <ul>
+                  {article.items.map((item, index) => (
+                    <li key={index}>・{item}</li>
+                  ))}
+                </ul>
+              )}
+            </div>
+          </React.Fragment>
+        ))}
+      </dl>
+    </section>
   );
 }
